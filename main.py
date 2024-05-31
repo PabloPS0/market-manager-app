a13817875c6687de136b187573a9292196bf9ec8
Pablo <pablopierry2021@gmail.com>
2024-05-31 17:03:44 -0300

    chore: remove obsolete logging functionality
    - Removed the logging functionality that was no longer needed.
    - The functionality was replaced by a more efficient logging system.

--- main.py
+++ main.py
@@ -5,8 +5,3 @@ class Product:
         self.price = price
     def display_info(self):
         print(f'Nome: {self.name}, Código: {self.code}, Preço: R${self.price:.2f}')
-    def add_data(self):
-        self.data = input("Insira a data de fabricação (DD/MM/YYYY): ")
-        print(f'Data de fabricação: {self.data}')
-
-data()
