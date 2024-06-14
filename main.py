947444fc8466fd3a457f719a6362d3455deeb930
PabloPS0 <pablopierry.github.98@gmail.com>
2024-06-14 01:19:42 -0300

    feat: Add product addition functionality

--- main.py
+++ main.py
@@ -3,5 +3,21 @@ class Product:
         self.name = name
         self.code = code
         self.price = price
+
     def display_info(self):
         print(f'Nome: {self.name}, Código: {self.code}, Preço: R${self.price:.2f}')
+
+# Lista para armazenar produtos
+products = []
+
+add = input(f'Você quer adicionar um produto?(s/n) ').strip.upper
+
+while add == 'S':
+    name = input('Nome do produto: ')
+    code = input('Código do produto: ')
+    price = float(input('Preço do produto:R$ '))
+
+    product = Product(name, code, price)
+    products.append(product) # Adiciona o produto a lista
+
+    add = input(f'Você quer adicionar outro produto?(s/n) ').strip.upper
